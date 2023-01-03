87606dd8f4e65361c198784a53800cec0fedfd31
vinod-rajasekaran <flans-boules0n@icloud.com>
2023-01-03 20:59:02 +0530

    fixed issue if date is empty string

--- models/intermediate/zzz_case.sql
+++ models/intermediate/zzz_case.sql
@@ -21,14 +21,14 @@ select
     	   (_airbyte_data ->>  'closed')::boolean as closed,
     	   _airbyte_data ->>  'properties_anc_closereason' as anc_closereason,
     	   _airbyte_data ->>  'properties_high_risk_preg' as high_risk_preg,
-    	   date(_airbyte_data ->>  'properties_lmpdate') as lmpdate,
+    	   date(NULLIF(_airbyte_data ->>  'properties_lmpdate','')) as lmpdate,
     	   _airbyte_data ->>  'properties_referral' as referral,
-    	   date(_airbyte_data ->>  'properties_referraldate') as referral_date,
+    	   date(NULLIF(_airbyte_data ->>  'properties_referraldate','')) as referral_date,
     	   _airbyte_data ->>  'properties_referralplace' as referral_place,
     	   _airbyte_data ->>  'properties_referralreason' as referral_reason,
     	   _airbyte_data ->>  'properties_referralcategory' as referral_category,
     	   _airbyte_data ->>  'properties_prev_pregoutcome' as prev_pregoutcome,
-           date(_airbyte_data ->> 'properties_finalWDOB') as  finalwdob,
+           date(NULLIF(_airbyte_data ->> 'properties_finalWDOB','')) as  finalwdob,
            (_airbyte_data ->> 'properties_gravida_count')::int as  gravida_count,
            _airbyte_data ->> 'properties_referral_followupname' as  referral_followupname
 
