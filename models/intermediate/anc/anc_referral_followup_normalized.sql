74e111429ef95a14f915296ca485349219db36a1
vinod-rajasekaran <flans-boules0n@icloud.com>
2023-02-08 23:06:36 +0530

    Referral table fixed to use case instead of referral followup form
    Referral case data can be used ot link to woman data and provide direct link to pending followups - this is not possible to do from just the woman ANC case table.

--- models/intermediate/anc/anc_referral_followup_normalized.sql
+++ models/intermediate/anc/anc_referral_followup_normalized.sql
@@ -8,21 +8,28 @@
 ) }}
 
 SELECT 
-(_airbyte_data ->> 'id') as id,
-date(NULLIF(_airbyte_data -> 'form' ->> 'followupdate','')) as followup_date, 
-(_airbyte_data -> 'form' ->> 'statustreatment') as status_treatment,
-_airbyte_data -> 'form' ->> 'womanid' as womanid,
-_airbyte_data -> 'form' ->> 'referralcategory' as referral_category,
-date(NULLIF(_airbyte_data -> 'form' ->> 'referraldate','')) as referral_date,
-_airbyte_data -> 'form' ->> 'referral_placecat1' AS referral_place,
-_airbyte_data -> 'form' ->> 'referral_reasoncat1' as referral_reason,
-_airbyte_data -> 'form' ->> 'followup' as followup_required,
+_airbyte_data ->> 'id' as id,
+_airbyte_data -> 'properties' ->> 'womanid' as womanid,
+(_airbyte_data -> 'closed')::boolean as referral_closed,
+date(NULLIF(_airbyte_data -> 'properties' ->> 'followupdate','')) as followup_date, 
+_airbyte_data -> 'properties' ->> 'statustreatment' as status_treatment,
+_airbyte_data -> 'properties' ->> 'referralcategory' as referral_category,
+date(NULLIF(_airbyte_data -> 'properties' ->> 'referraldate','')) as referral_date,
+COALESCE(NULLIF(_airbyte_data -> 'properties' ->> 'referral_placecat1',''),
+NULLIF(_airbyte_data -> 'properties' ->> 'referral_placecat2',''),
+NULLIF(_airbyte_data -> 'properties' ->> 'referral_placecat3','')) as referral_place,
+COALESCE(NULLIF(_airbyte_data -> 'properties' ->> 'referralreason1',''),
+NULLIF(_airbyte_data -> 'properties' ->> 'referralreason2',''),
+NULLIF(_airbyte_data -> 'properties' ->> 'referralreason3','')) as referral_reason,
+_airbyte_data -> 'properties' ->> 'followup' as followup_required,
+CASE WHEN date(NULLIF(_airbyte_data -> 'properties' ->> 'followupdate','')) IS NOT NULL THEN 'Yes'
+ELSE 'No'
+END AS referral_followed_up,
 _airbyte_ab_id,
 _airbyte_emitted_at
-from {{ source('commcare_anc', 'raw_referral_followup') }} 
-WHERE
-(_airbyte_data -> 'form' ->> 'referralcategory') = 'ANC/PNC' 
-AND (_airbyte_data ->> 'archived')::boolean = false
+from {{ source('commcare_anc', 'raw_case') }}
+where (_airbyte_data -> 'properties' ->> 'case_type') = 'sneharefollwuptemp'
+AND (_airbyte_data -> 'properties' ->> 'referralcategory') = 'ANC/PNC' 
 /*remove test data */
-AND  (_airbyte_data -> 'form' ->> 'coid') NOT IN ('00','001') 
-AND  (_airbyte_data -> 'form' ->> 'coid') <> ''
+AND  (_airbyte_data -> 'properties' ->> 'coid') NOT IN ('00','001') 
+AND  (_airbyte_data -> 'properties' ->> 'coid') <> ''
--- models/production/raw/anc_referral_followup.sql
+++ models/production/raw/anc_referral_followup.sql
@@ -2,8 +2,7 @@
   materialized='table'
 ) }}
 
-SELECT referral_followups.*,
-c.clusterid,c.clustername,c.program_code,c.coid,c.womanname,c.aww_number,c.hh_number,c.anc_closed
-FROM {{ref('anc_referral_followup_duplicates_removed')}} AS referral_followups 
-LEFT JOIN {{ref('anc_case_duplicates_removed')}} AS c
-ON referral_followups.womanid=c.womanid
+SELECT referrals.*, c.clusterid,c.clustername,c.program_code,c.coid,c.womanname,c.aww_number,c.hh_number,c.anc_closed 
+from {{ref('anc_referral_followup_duplicates_removed')}} as referrals
+LEFT JOIN {{ref('anc_case_duplicates_removed')}} as c
+ON referrals.womanid=c.womanid
