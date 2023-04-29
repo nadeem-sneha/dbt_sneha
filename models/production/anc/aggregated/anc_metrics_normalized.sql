cb631932c3643a55328952a55e3fb599b34cf05c
vinod-rajasekaran <flans-boules0n@icloud.com>
2023-04-29 09:30:21 +0530

    fixed typos

--- models/production/anc/aggregated/anc_metrics_normalized.sql
+++ models/production/anc/aggregated/anc_metrics_normalized.sql
@@ -4,17 +4,17 @@
 
 select date_month,program_code,clustername,
 unnest(array[
-    'ANC Women Visited',
-    'ANC women visted by COs',
-    'ANC Women Visited by Volunteers',
-    'ANC Volunteer Visits with Referrals',
-    'ANC CO Visits with Referrals',
-    'Volunteers involved in Referrals'
-    'Referrals for THR',
-    'Visits with ANC women reporting THR usage',
-    'Institutional Delivery',
-    'Anemia Tested',
-    'Early registration'
+    '1. ANC Women Visited',
+    '2. ANC women visted by COs',
+    '3. ANC Women Visited by Volunteers',
+    '4. ANC Volunteer Visits with Referrals',
+    '5. ANC CO Visits with Referrals',
+    '6. Volunteers involved in Referrals',
+    '7. Referrals for THR',
+    '8. isits with ANC women reporting THR usage',
+    '9. Institutional Delivery',
+    '10.Anemia Tested',
+    '11. Early registration'
     ]) AS indicator,
 unnest(array[
     pct_total_visits,
@@ -27,8 +27,10 @@ unnest(array[
     pct_thr_visits,
     pct_institutional_delivery,
     pct_anemia_tested,
-    pct_early_registration]) AS score
+    pct_early_registration
+    ]) AS score
 FROM {{ref('anc_metrics')}}
+where date_month IS NOT NULL
 
 
 
