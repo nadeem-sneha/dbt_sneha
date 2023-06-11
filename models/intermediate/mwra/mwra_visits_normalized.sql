3595fb5fbf9ce10558e04037f4c23b35e30ff417
vinod-rajasekaran <flans-boules0n@icloud.com>
2023-06-11 11:26:37 +0530

    Fix schema to be same as other models in main branch

--- models/intermediate/mwra/mwra_visits_normalized.sql
+++ models/intermediate/mwra/mwra_visits_normalized.sql
@@ -2,7 +2,7 @@
     config(
         materialized="table",
         indexes=[{"columns": ["_airbyte_ab_id"], "type": "hash"}],
-        schema="dev_goalkeep",
+        schema="intermediate",
     )
 }}
 
